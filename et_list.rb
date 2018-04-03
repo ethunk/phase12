f28c27c1b5e56eb0aa2b0f9c1668b583ba10c659
ethunk <ethunk>
2018-04-03 17:49:47 -0400

    init commit with et_list

---
+++ et_list.rb
@@ -0,0 +1,50 @@
+['big-o-notation',
+'duplicate-element-counter',
+'search-algorithms',
+'minimum-coins',
+'optimal-guesser',
+'linked-list',
+'linked-list-challenge',
+'bracket-matcher',
+'stacks',
+'caesar-cipher',
+'queues',
+'minimum-consecutive-integers',
+'sorting-algorithms',
+'greatest-common-factor',
+'word-counter',
+'recursion',
+'fibonacci',
+'factorial',
+'continue-your-online-learning',
+'reading-list',
+'what-a-web-developer-should-know'].each do |slug|
+  system "et get ${slug}"
+end
+
+
+
+
+# slug                                                             | title                                                             | type
+# ---------------------------------------------------------------------------------------------------------------------------------------------------
+#  big-o-notation                                                   | Big O Notation                                                    | article
+#  duplicate-element-counter                                        | Duplicate Element Counter                                         | challenge
+#  search-algorithms                                                | Search Algorithms                                                 | article
+#  minimum-coins                                                    | Minimum Coins                                                     | challenge
+#  optimal-guesser                                                  | Optimal Guesser                                                   | challenge
+#  linked-list                                                      | Linked List                                                       | article
+#  linked-list-challenge                                            | Linked List Challenge                                             | challenge
+#  bracket-matcher                                                  | Bracket Matcher                                                   | challenge
+#  stacks                                                           | Stacks                                                            | article
+#  caesar-cipher                                                    | Caesar Cipher                                                     | challenge
+#  queues                                                           | Queues                                                            | challenge
+#  minimum-consecutive-integers                                     | Minimum Consecutive Integers                                      | challenge
+#  sorting-algorithms                                               | Sorting Algorithms                                                | article
+#  greatest-common-factor                                           | Greatest Common Factor                                            | challenge
+#  word-counter                                                     | Word Counter                                                      | challenge
+#  recursion                                                        | Recursion                                                         | article
+#  fibonacci                                                        | Fibonacci                                                         | exercise
+#  factorial                                                        | Factorial                                                         | exercise
+#  continue-your-online-learning                                    | Continue Your Online Learning                                     | article
+#  reading-list                                                     | Reading List                                                      | article
+#  what-a-web-developer-should-know                                 | What a Web Developer Should Know                                  | article
