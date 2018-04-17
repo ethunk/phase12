972ff8f5e8934cd7ffabd5693edb27d240ea08bf
ethunk <ethunk>
2018-04-16 20:50:46 -0400

    finished, fibonacci(1000) less than 1 sec runtime

---
+++ recursion/fibonacci.rb
@@ -0,0 +1,11 @@
+# YOUR CODE HERE
+require 'pry'
+require 'benchmark'
+
+@cache = {}; @cache[1] = 1; @cache[2] = 1
+
+def fibonacci(n)
+  @cache[n] ||= fibonacci(n - 1) + fibonacci(n - 2)
+end
+
+binding.pry
